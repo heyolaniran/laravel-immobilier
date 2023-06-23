6efe68dc3106e74f684b5317f7a216630296a584
heyolaniran <olaniran.abd@gmail.com>
2023-06-23 17:29:52 +0100

    fix: refactoring listing cards

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -10,13 +10,8 @@
 
 <div class="row">
     @foreach($properties as $property)
-        <div class="card" style="width: 18rem;">
-            <img src="..." class="card-img-top" alt="...">
-            <div class="card-body">
-                <h5 class="card-title">{{ $property->titre }}</h5>
-                <p class="card-text">{{ $property->description }}</p>
-                <a href="{{ route('admin.properties.show' , $property) }}" class="btn btn-primary">Voir </a>
-            </div>
+        <div class="col">
+            @include('components.properties.card' , ['property' =>$property])
         </div>
     @endforeach
 </div>
