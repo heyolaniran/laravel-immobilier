42c1654c471e6af57ebae83b645e926cb3f6f890
heyolaniran <olaniran.abd@gmail.com>
2023-06-26 14:49:01 +0100

    feat: fin de la page de details sur les biens

--- resources/views/property/show.blade.php
+++ resources/views/property/show.blade.php
@@ -12,6 +12,44 @@
     <div class="text-primary font-weight-bold mt-2">
        <h3>{{number_format($property->price, thousands_separator: ' ')}} XOF</h3> 
     </div>
+
+    <hr>
+    <div class="container mt-4">
+        <form action="" method="post">
+            @csrf
+            <div class="row">
+                @include('admin.shared.input' , ['class' => 'col' , 'name'=>'prenom' , 'label' => 'Prenoms'])
+                @include('admin.shared.input' , ['class' => 'col' , 'name'=>'nom' , 'label' => 'Nom'])
+            </div>
+            <div class="row">
+            @include('admin.shared.input' , ['class' => 'col' , 'name'=>'telephone' ])
+                @include('admin.shared.input' , ['class' => 'col' , 'type'=>'email', 'name'=>'email' , 'label' => 'E-mail'])
+            </div>
+
+            @include('admin.shared.input', ['type' =>'textarea' , 'class' =>'col' , 'name' =>'message'])
+
+            <button class="btn btn-primary btn-md">Nous contacter </button>
+        </form>
+    </div>
+
+    <div class="mt-4">
+        {!! nl2br($property->description) !!}
+    </div>
+
+    <div class="mt-4">
+        <h4>Caractéristiques</h4>
+    </div>
+
+    <div class="mt-4 mb-4">
+        <h4>Spécificités</h4>
+        <div class="mt-4">
+            <ul class="list-group">
+            @foreach($property->options as $option)
+            <li href="#" class="list-group-item ">{{$option->name}}</li>
+            @endforeach
+            </ul>
+        </div>
+    </div>
 </div>
 
 
